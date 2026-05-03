4362846013249d580cd655cb2bfb4bcf16c97335
Yash <yashmy68711@gmail.com>
2026-05-03 22:49:05 +0530

    Update Contact Book .py

--- Contact Book .py
+++ Contact Book .py
@@ -12,31 +12,50 @@ while True:
         name = input("Enter name: ")
         phone = input("Enter phone: ")
 
-        if name.strip() == "" or phone.strip() == "":
+        if name == "" or phone == "":
             print("Invalid input")
+        elif name.lower() in (n.lower() for n in contacts):
+            print("Contact already exists")
         else:
             contacts[name] = phone
             print("Contact added!")
-
+            
     elif choice == "2":
         if len(contacts) == 0:
             print("No contacts yet")
         else:
             print("\nContacts:")
-            for name, phone in contacts.items():
-                print(name, ":", phone)
+            for i, (name, phone) in enumerate(contacts.items(), 1):
+                print(f"{i}. {name} : {phone}")
 
     elif choice == "3":
         search = input("Enter name to search: ")
+        found = False
+        for name, phone in contacts.items():
+            if name.lower() == search:
+                print(name, ":", phone)
+                found = True
+                break
 
-        if search in contacts:
-            print(search, ":", contacts[search])
-        else:
+        if not found:
             print("Contact not found")
 
     elif choice == "4":
+       delete = input("Enter name to delete: ").strip().lower()
+
+        found = False
+        for name in list(contacts.keys()):
+            if name.lower() == delete:
+                del contacts[name]
+                print("Contact deleted!")
+                found = True
+                break
+
+        if not found:
+            print("Contact not found")
+
+    elif choice == "5":
         print("Bye!")
         break
-
     else:
         print("Wrong choice")
